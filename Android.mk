d294c460cd58852b9b2050ee265646c6c3559b6a
Koya.cc <coezkanli@gmail.com>
2026-04-14 23:37:35 +0200

    Aktualisieren von Android.mk

--- Android.mk
+++ Android.mk
@@ -1,8 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-# ✅ Force C++ STL
-APP_STL := c++_static
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := libdobby
 LOCAL_SRC_FILES := core/cheat/hooks/dobby/arm64-v8a/libdobby.a
@@ -26,13 +23,10 @@ LOCAL_SRC_FILES := main.cpp \
 LOCAL_CFLAGS := -O2 -ffast-math -ffunction-sections -fdata-sections
 LOCAL_CFLAGS += -fvisibility=hidden -fvisibility-inlines-hidden
 
-# ✅ IMPORTANT FIXES
 LOCAL_CPPFLAGS := -std=c++17 -O2 -frtti -fexceptions
 
-# ❌ REMOVE libc++ from here
 LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv3
 
-# ✅ CORRECT linking
-LOCAL_STATIC_LIBRARIES := libdobby c++_static
+LOCAL_STATIC_LIBRARIES := libdobby
 
 include $(BUILD_SHARED_LIBRARY)
